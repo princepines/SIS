bc19cee00fa26af7fbe43518f49588da6c6f7a13
Frizth Lyco Tatierra (princepines/Lycol50) <frizth_tatierra@informatics.edu.ph>
2023-04-15 01:14:48 +0800

    new features

---
+++ forms/teacher/advisory.php
@@ -0,0 +1,5 @@
+<?php
+// Check Class Advisories for assigned subjects
+require_once $_SERVER['DOCUMENT_ROOT'] . '/SIS/config.php';
+
+?>
---
+++ forms/teacher/grades.php
@@ -0,0 +1,4 @@
+<?php
+// records grades from specific subject and student
+require_once $_SERVER['DOCUMENT_ROOT'] . '/SIS/config.php';
+?>
